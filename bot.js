4967c86825a714a56a88873fe9b679d8a37250e8
gmflmadden <lowrider8071@yahoo.com>
2016-03-30 17:33:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/;botRegexGOAT = /^\/GOAT/
-      botRegexAd=/^\/advance/;botRegexBroncos = /^\/Broncos/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
+      botRegexAd=/^\/advance/;botRegexBroncos = /^\/Broncos/;botRegexSaints = /^\/Saints/;botRegexColts = /^\/Colts/;botRegexVikings = /^\/Vikings/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
@@ -48,6 +48,21 @@ function respond() {
     this.res.writeHead(200);
     postMessage("https://www.twitch.tv/xx3windy1city2xx");
     this.res.end();
+  } 
+      else if(request.text && botRegexSaints.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://www.twitch.tv/ford504");
+    this.res.end();
+  } 
+      else if(request.text && botRegexColts.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/panthersnation7");
+    this.res.end();  
+  } 
+      else if(request.text && botRegexVikings.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/puba1978");
+    this.res.end();  
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
     this.res.writeHead(200);
